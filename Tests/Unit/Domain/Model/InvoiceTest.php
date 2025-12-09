8ec66aa179e98f9579a4b30d8c440f5fc9ebb034
Kay Strobach <info@kay-strobach.de>
2025-12-09 15:18:50 +0100

    [TASK] add test wrapper
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

---
+++ Tests/Unit/Domain/Model/InvoiceTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace KayStrobach\Invoice\Tests\Unit\Domain\Model;
+
+/*
+ * This file is part of the KayStrobach.Invoice package.
+ */
+
+/**
+ * Testcase for Invoice
+ */
+class InvoiceTest extends \Neos\Flow\Tests\UnitTestCase
+{
+
+    /**
+     * @test
+     */
+    public function makeSureThatSomethingHolds()
+    {
+        $this->markTestIncomplete('Automatically generated test case; you need to adjust this!');
+
+        $expected = 'Foo';
+        $actual = 'Foo'; // This should be the result of some function call
+        $this->assertSame($expected, $actual);
+    }
+}
