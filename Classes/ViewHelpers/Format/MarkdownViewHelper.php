bb5c2613ca20341e66b83997862ab8d6084a52e1
Kay Strobach <info@kay-strobach.de>
2025-12-11 20:14:28 +0100

    [TASK] add markdown renderer
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

---
+++ Classes/ViewHelpers/Format/MarkdownViewHelper.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KayStrobach\Invoice\Format\ViewHelpers;
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\Autolink\AutolinkExtension;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\DescriptionList\DescriptionListExtension;
+use League\CommonMark\Extension\Footnote\FootnoteExtension;
+use League\CommonMark\Extension\Strikethrough\StrikethroughExtension;
+use League\CommonMark\Extension\Table\TableExtension;
+use League\CommonMark\Extension\TaskList\TaskListExtension;
+use League\CommonMark\MarkdownConverter;
+use TYPO3Fluid\Fluid\Core\Compiler\TemplateCompiler;
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\ViewHelperNode;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class MarkdownViewHelper extends AbstractViewHelper
+{
+    /**
+     * @var boolean
+     */
+    protected $escapeChildren = false;
+
+    /**
+     * Specifies whether the escaping interceptors should be disabled or enabled for the render-result of this ViewHelper
+     * @see isOutputEscapingEnabled()
+     *
+     * @var bool
+     * @api
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * Initialize the arguments.
+     *
+     * @return void
+     * @api
+     */
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('value', 'string', 'string to format', false, null);
+    }
+
+    /**
+     * Escapes special characters with their escaped counterparts as needed using PHPs strip_tags() function.
+     *
+     * @return mixed
+     * @see http://www.php.net/manual/function.strip-tags.php
+     * @api
+     */
+    public function render()
+    {
+        $value = $this->arguments['value'];
+
+        if ($value === null) {
+            $value = $this->renderChildren();
+        }
+        if (is_object($value) && method_exists($value, '__toString')) {
+            $value = $value->__toString();
+        } elseif (!is_string($value)) {
+            return $value;
+        }
+
+        $config = [
+            //'html_input' => 'strip',
+            'allow_unsafe_links' => false,
+            'table' => [
+                'wrap' => [
+                    'enabled' => false,
+                    'tag' => 'div',
+                    'attributes' => [],
+                ],
+                'alignment_attributes' => [
+                    'left'   => ['align' => 'left'],
+                    'center' => ['align' => 'center'],
+                    'right'  => ['align' => 'right'],
+                ],
+            ],
+        ];
+
+        // Add this extension
+        $environment = new Environment($config);
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new TaskListExtension());
+        $environment->addExtension(new AutolinkExtension());
+        $environment->addExtension(new DescriptionListExtension());
+        $environment->addExtension(new StrikethroughExtension());
+        $environment->addExtension(new TableExtension());
+
+        // Instantiate the converter engine and start converting some Markdown!
+        $converter = new MarkdownConverter($environment);
+
+        return $converter->convert($value);
+    }
+}
