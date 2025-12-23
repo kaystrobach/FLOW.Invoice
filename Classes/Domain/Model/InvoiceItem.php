1de6be7344d4f5423f8f05a3a7dc89fca77cad67
Kay Strobach <info@kay-strobach.de>
2025-12-23 12:15:50 +0100

    [TASK] add info and stornorechnung
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- Classes/Domain/Model/InvoiceItem.php
+++ Classes/Domain/Model/InvoiceItem.php
@@ -264,6 +264,7 @@ class InvoiceItem
 
     public function calculateTotal()
     {
+        // 1595,36 * 19 > 313,1184 --> runden kaufmännisch ab 5 aufrunden --> 303,12
         $this->total->setValue($this->getAmount() * $this->getSinglePrice()->getValue());
     }
 
