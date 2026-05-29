091542be4d8be30a08f8e0ba6e5e1c30e4de2fdf
Kay Strobach <info@kay-strobach.de>
2026-05-29 15:43:17 +0200

    [FEATURE] add commandcontroller to unlock an invoice again
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- Classes/Command/InvoiceCommandController.php
+++ Classes/Command/InvoiceCommandController.php
@@ -8,6 +8,7 @@ use Neos\Flow\Cli\CommandController;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
+use Neos\Flow\ResourceManagement\ResourceManager;
 
 /**
  * @Flow\Scope("singleton")
@@ -20,11 +21,25 @@ class InvoiceCommandController extends CommandController
     #[Flow\Inject()]
     protected PersistenceManagerInterface $persistenceManager;
 
+    #[Flow\Inject()]
+    protected ResourceManager $resourceManager;
+
+    public function statusCommand(Invoice $invoice)
+    {
+        $this->outputLine($invoice->isChangeable() ? 1 : 0);
+    }
+
     public function resetCommand(Invoice $invoice)
     {
         $invoice->setChangeable(true);
         $invoice->getNumber()->resetNumber();
-        // $invoice->resetOriginalResource();
+
+        $resource = $invoice->getOriginalResource();
+        if ($resource !== null) {
+            $this->resourceManager->deleteResource($resource);
+            $invoice->resetOriginalResource();
+        }
+
 
         $this->invoiceRepository->update($invoice);
         $this->persistenceManager->persistAll();
